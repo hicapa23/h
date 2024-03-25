227d8edd28a05b7c0b0a38306e53f941e45d5ee9
hicapa23 <145991782+hicapa23@users.noreply.github.com>
2024-03-25 09:50:53 +0100

    Update ZahlenUbersetzer.java

--- src/ZahlenUbersetzer.java
+++ src/ZahlenUbersetzer.java
@@ -6,37 +6,20 @@ public class ZahlenUbersetzer {
         String romicheZahl;
         int zahl;
 
-        System.out.println("Wuenschen Sie eine Übersetzung von Zahlen zu arabischen (Arabisch) oder romischen Zahlen (Romanisch)?");
-        var auswahl = scanner.nextLine();
+        System.out.println("Geben Sie eine ganze Zahl zwischen 1 und 3999:");
+        zahl = scanner.nextInt();
 
-        if ("Romanisch".equalsIgnoreCase(auswahl)) {
-             System.out.println("Geben Sie eine ganze Zahl zwischen 1 und 3999:");
-            zahl = scanner.nextInt();
-
-            if (zahl > 1 ){
-                romicheZahl= konvertionZuRomischenZahlen(zahl);
-                System.out.println(zahl+" In romischen Zahlen ist: "+romicheZahl);
-            }
-            
-           
-        }
-
-        else{
-            System.out.println("Geben Sie eine ganze Zahl zwischen 1 und 3999:");
-            zahl = scanner.nextInt();
-
-            if (zahl > 1 && zahl < 3999){
-                romicheZahl= konvertionZuArabischenZahlen(zahl);
-                System.out.println(zahl+" In romischen Zahlen ist: "+romicheZahl);
-            }
-            
-            else {
-                
-                System.out.println("Die nummer soll im Rang sein");
-            }
-
+        if (zahl > 1 && zahl < 3999){
+            romicheZahl= konvertionZuRomischenZahlen(zahl);
+            System.out.println(zahl+" In romischen Zahlen ist: "+romicheZahl);
         }
         
+        else {
+            
+            System.out.println("Die nummer soll im Rang sein");
+        }
+
+
     }
 
     public static String konvertionZuRomischenZahlen(int zahl ){
@@ -59,25 +42,5 @@ public class ZahlenUbersetzer {
         return tausende[indexTausende] + hunderter[indexHunderter] + zehner[indexZehner] + einheiten [indexEinheiten];
 
     }
-
-    public static String konvertionZuArabischenZahlen(int zahl ){
-
-        String []  einheitenA = {".", "واحد", "اثنان", "ثلاثة", "أربعة", "خمسة", "ستة", "سبعة", "ثمانية", "تسعة", "عشرة"};
-        String []  zehnerA =  {"", "عشرون", "ثلاثون", "أربعون", "خمسون", "ستون", "سبعون", "ثمانون", "ت  !سعون", "مئة"};
-        String []  hunderterA =  {"", "مئة", "مئتان", "ثلاثمئة", "أربعمئة", "خمسمئة", "ستمئة", "سبعمئة", "ثمانمئة", "تسعمئة", "ألف"};
-        String []  tausendeA =  {"", "ألف", "ألفان", "ثلاثة آلاف", "أربعة آلاف", "خمسة آلاف", "ستة آلاف", "سبعة آلاف", "ثمانية آلاف", "تسعة آلاف", "عشرة آلاف"};
-
-        int indexEinheitenA, indexZehnerA, indexHunderterA, indexTausendeA;
-
-        indexTausendeA = zahl / 1000;
-        zahl = zahl % 1000;
-        indexHunderterA = zahl  / 100;
-        zahl = zahl % 100;
-        indexZehnerA = zahl / 10;
-        zahl = zahl % 10;
-        indexEinheitenA = zahl;
-
-        return tausendeA[indexTausendeA] + hunderterA[indexHunderterA] + zehnerA[indexZehnerA] + einheitenA [indexEinheitenA]    ;  
-    }
-
 }
+        
